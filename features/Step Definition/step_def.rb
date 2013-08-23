52d3d20816f03eae7f1499a5eabddeb2ef0f6789
Swati Singh <swati.singh@macys.com>
2013-08-23 10:53:11 +0530

    initial commit

---
+++ features/Step Definition/step_def.rb
@@ -0,0 +1,15 @@
+Given(/^a board like this$/) do |table|
+  @board = table.raw
+  print @board
+end
+
+When(/^player x plays in row (\d+), column (\d+)$/) do |row, col|
+  @board[row.to_i][col.to_i] = "x"
+  puts @board
+end
+
+Then(/^the board should look like this:$/) do |expected_table|
+  expected_table.diff!(@board)
+end
+
+
